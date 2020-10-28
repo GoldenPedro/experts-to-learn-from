0c5b16a4c347ce4aa990e40a0bda684621b52558
Alegar917 <argarcia6@dons.usfca.edu>
2020-10-28 19:21:53 -0400

    completed passport setup and JWT token

--- server/server.js
+++ server/server.js
@@ -4,36 +4,23 @@ const app = express(); // create express app
 const path = require("path");
 const http = require('http');
 // const mongoose = require('mongoose');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 var cors = require('cors')
 // const bcrypt = require('bcrypt');
-const session = require('express-session');
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const jwt = require('jsonwebtoken');
+
 
 const URI = process.env.MONGO_URI;
-
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
-
-
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
-
 app.use(passport.initialize());
-app.use(passport.session());
 
-// MongoClient.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true },function(err, client){
-//   if(err) console.log(err);
-//   console.log("connection successful");
-//   const test = client.db('mydatabase').collection('users');
 
 async function data(callback){
     const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -52,22 +39,46 @@ async function data(callback){
       }
     }
 
-  // function ensureAuthenticated(req, res, next) {
-  //     if (req.isAuthenticated()) {
-  //       return next();
-  //     }
-  //     res.redirect('/');
-  //   }
+  function ensureAuthenticated(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      console.log("lol you tried me")
+      res.redirect('/');
+    }
 
   data(async (client) => {
     const users = await client.db('mydatabase').collection('users');
     // const experts = await client.db('mydatabase').collection('experts');
 
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+    passport.deserializeUser((id, done) => {
+      users.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+        if (err) return console.log(err);
+        done(null, doc);
+      });
+    });
 
+    passport.use(new LocalStrategy({
+      usernameField: 'email',   
+      passwordField: 'password'
+    },
+      function (username, password, done) {
+        users.findOne({ email: username }, function (err, user) {
+          console.log('User '+ user +' attempted to log in.');
+          if (err) { return done(err); }
+          if (!user) { return done(null, false); }
+          if (password !== user.password) { return done(null, false); }
+          return done(null, user);
+        });
+      }
+    ));
+    
     app.post("/api/users", (req, res, next) => {
-      // console.log(req.body)
-
       // const hash = bcrypt.hashSync(req.body.password, 12);
+
       users.findOne({ email: req.body.email }, function (err, user) {
         if (err) {
           next(err);
@@ -80,34 +91,63 @@ async function data(callback){
 
           return res.json({
             success: true,
-            redirectUrl: '/'
+            redirectUrl: '/login'
           });
         } else {
           users.insertOne({ email: req.body.email, password: req.body.password }, (err, doc) => {
             if (err) {
-      
+              return res.json({
+                success: false,
+                redirectUrl: '/'
+              });
             } else {
-              next(null, doc.ops[0]);
+              next(null, doc.ops[0]);   
             }
           });
         }
       });
-    
-      // users.insertOne(req.body, function(err, res) {
-      //     if (err) throw err;
-      //     console.log("1 user inserted");
-      //   })
-      
-    });
-    
-    app.post("/api/login", (req, res) => {
-      console.log(req.body)
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      // res.redirect('/profile');
     });
 
-    
+    // app.get('/logout', (req, res) => {
+    //   req.logout();
+    //   res.redirect('/');
+    // });
+
+    app.route('/lol').get(ensureAuthenticated, (req, res) => {
+      //res.render();
+    });
+
     // app.post("/api/login", (req, res) => {
-    //   passport.authenticate('local', { failureRedirect: '/' }), (req, res) => { 
-    //   }});
+    //   console.log(req.body)
+    //   passport.authenticate('local'), (req, res) => {
+    //     console.log("hello")
+    //     // console.log(req.body)
+    //   }
+    // });
+
+    app.route("/api/login").post(passport.authenticate('local',(err, user, info) => {
+      if(err){
+        console.log(err)
+      }
+      if(info !== undefined){
+        console.log(info.message);
+        //maybe send the info
+      }
+      else{
+        console.log(user)
+        const token = jwt.sign({id: user.email}, process.env.SESSION_SECRET)
+        console.log(token);
+      }
+      }), (req, res) => {
+
+    });
+
+
+    
 
    
     app.use((req, res, next) => {
